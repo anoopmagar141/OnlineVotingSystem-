316d6a3bd6d18144b2306ac3994a1cfbc2c0ce72
[Anup  Magar] <am6956275@gmail.com>
2025-01-12 21:25:15 +0545

    starting the session and checking connection

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+session_start(); // Start a session
+$conn = new mysqli("localhost", "root", "", "voting_system");
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
